766e167d47bc43575cbe28ad025a84a28c1727a9
Giles Van Gruisen <gilesvangruisen@me.com>
2014-05-27 12:33:27 -0400

    begin with empty uiviewcontroller subclass

---
+++ GVGPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  GVGPageViewController.h
+//  ForgeModule
+//
+//  Created by Giles Van Gruisen on 5/27/14.
+//  Copyright (c) 2014 Trigger Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GVGPageViewController : UIViewController
+
+@end
